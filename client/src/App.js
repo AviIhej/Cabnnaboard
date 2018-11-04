c9a4611b88e4363aa62a7c1777c2ff41e2093da8
Avi Ihej <aviihej@gmail.com>
2018-11-03 23:50:18 -0400

    delete uneccesary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 // or you can say this: import {default as ApolloClient } from 'apollo-boost';
-import { gql } from 'graphql-tag';
+import gql from 'graphql-tag';
 import { ApolloProvider } from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
+import { HttpLink } from 'apollo-link-http';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000'
+  link: new HttpLink({ uri: 'http://localhost:4000' })
 })
+
+const query = gql`
+  {
+    jobs{
+      id
+      companyId
+      title
+      description
+    }
+  }
+`
+
+client
+  .query({
+    query: query
+  })
+  .then(res => console.log(res))
 
 class App extends Component {
   render() {
