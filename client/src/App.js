af793895f8ce210d3352eb35563c7440e75c2d16
Avi Ihej <aviihej@gmail.com>
2018-11-11 17:13:30 -0500

    add prisma-server folder with prisma bindings for connection to database

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import ApolloClient from 'apollo-boost';
-// or you can say this: import {default as ApolloClient } from 'apollo-boost';
-import gql from 'graphql-tag';
-import { ApolloProvider, Query } from 'react-apollo';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+// or you can say t his: import {default as ApolloClient } from 'apollo-boost';
+import gql from "graphql-tag";
+import { ApolloProvider, Query } from "react-apollo";
+// import logo from "./logo.svg";
+import "./App.css";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000'
-})
+  uri: "http://localhost:4466"
+});
 
 const getJobs = gql`
- { 
-    jobs{
+  query {
+    jobs {
       id
-      companyId
       title
-      description
+      status
+      companyName
+      category
     }
-}
-`
+  }
+`;
 
 // client
 //   .query({
@@ -35,16 +35,30 @@ class App extends Component {
       <ApolloProvider client={client}>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            {/* <img src={logo} className="App-logo" alt="logo" />
             <p>
               Edit <code>src/App.js</code> and save to reload.
-            </p>
+            </p> */}
+            {/* <div className="title"> */}
+              <h3 style={{float: "right"}}>CannaBoard 🍁</h3>
+              <button>POST JOB FOR $99</button>
+            {/* </div> */}
+            <h1>Post a Job Find a job in the Canadian Cannabis Industry 🇨🇦</h1>
+            <p>Have cannabis jobs sent directly to your inbox!</p>
             <Query query={getJobs}>
               {({ loading, data }) => {
-                if(loading) return <p>Loading...</p>
+                if (loading) return <p>Loadi∏ng...</p>;
                 // instead of saying data.getJobs, we can say
-                 const { jobs } = data
-                 return jobs.map(job => <p key={job.id}>{job.title}</p>)
+                const { jobs } = data;
+                return jobs.map(job => (
+                  <ul key={job.id}>
+                    {/* <li>{job.id}</li> */}
+                    <li>{job.title}</li>
+                    <li>{job.companyName}</li>
+                    <li>{job.category}</li>
+                    <li>{job.status}</li>
+                  </ul>
+                ));
               }}
             </Query>
           </header>
